4f98a60cb20e512989913546bc64aa2f29d89ec9
asadpisatel <berdibaev.asadbek@gmail.com>
2025-01-26 00:32:30 +0500

    changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const saltRounds = 10;
 
 const checkUserStatus = (req, res, next) => {
